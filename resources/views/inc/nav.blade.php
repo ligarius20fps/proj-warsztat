7f1582f2826243604f4b05843a5879a6104966f9
ligarius20fps <ligarius20fps@users.noreply.github.com>
2020-10-18 16:35:19 +0200

    Przetłumaczono formularz rejestracji i logowania

--- resources/views/inc/nav.blade.php
+++ resources/views/inc/nav.blade.php
@@ -18,7 +18,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Zaloguj') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Zaloguj się') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
@@ -35,7 +35,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Wyloguj się') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
