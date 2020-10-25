b5e349ee3280dcc6d86b6b93bb4bc3fe457c0bcd
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-25 16:39:21 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,11 @@
 
 <div class="container col-md-8">
     <div class="col-md-12">
-        <form class="input-group mb-4">
-                <input type="search" placeholder="Wpisz miasto/rodzaj usługi" aria-describedby="button-addon5" class="form-control">
+        <form class="input-group mb-4" action="/search" method="POST" role="search">
+            {{ csrf_field() }}
+                <input type="search" placeholder="Wpisz miasto/rodzaj usługi" aria-describedby="button-addon5" class="form-control" name="q">
                 <div class="input-group-append">
-                  <button class="btn btn-primary" type="button"><i class="fa fa-search"></i></button>
+                    <button class="btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
                 </div>
         </form>
     </div>
