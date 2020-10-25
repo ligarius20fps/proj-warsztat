7cc545c3623c14783da865f4b87c626a4bf93b10
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-25 20:11:47 +0100

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,15 +4,17 @@
  <h1>Wyniki wyszukiwania</h1>
 
  <button class="btn btn-primary">Filtruj</button>
- 
- <ul>
- @if(isset($details))
- @foreach($details as $workshop)
-
- <li>{​​{​​ $workshop->name }​​}​​</li>
-
+ <br><br>
+ <table class="table table-striped">
+     <thead></thead>
+     <tbody>
+ @foreach($workshops as $workshop)
+ <tr><td>
+    <a href="/workshops/{{ $workshop->id }}"> {​​{​​ $workshop->name }​​}​​ </a>
+ </td></tr>
  @endforeach
- @endif
- </ul>
+ <tbody>
+ </table>
+
 </div>
  @endsection
