d256092aac5088e4e5a32bb6f61872cfa475b1c5
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-26 15:53:25 +0100

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Workshops;
+use App\Models\City;
+use App\Models\Address;
+use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
     public function workshop_page(Workshops $workshop)
     {
         return view('workshop_page')->with('workshop',$workshop);
+    }
+    public function new_workshop()
+    {
+        $cities=City::all();
+        $workshop_types=DB::select("select * from workshop_types");
+        return view('new_workshop', ['cities'=>$cities, 'workshop_types'=>$workshop_types]);
+    }
+    public function add_workshop(Request $request)
+    {
+        Workshops::create($request->all());
+        Address::create($request->all());
+        return redirect('/account/workshops');
     }
 }
