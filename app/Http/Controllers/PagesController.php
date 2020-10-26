e370248e23c8275a2f92c7a4fbfa260673d92fe1
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-26 21:13:15 +0100

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -20,20 +20,43 @@ class PagesController extends Controller
         $workshop_types=DB::select("select * from workshop_types");
         return view('new_workshop', ['cities'=>$cities, 'workshop_types'=>$workshop_types]);
     }
-    public function add_workshop(/*Request $request*/)
+    public function add_workshop(Request $request)
     {
-         Address::create([
-            'street_name'=>request('street_name'),
-            'postal_code'=>request('postal_code'),
-            'building_number'=>request('building_number'),
-            'city_id'=>request('city_id')
+        $request->validate([
+            'street_name'=>'required',
+            'postal_code'=>'required',
+            'building_number'=>'required',
+            'name'=>'required',
+            'email'=>'required|email',
+            'workshop_type_id'=>'required',
         ]);
-        Workshops::create([
-            'name'=>request('name'),
-            'email'=>request('email'),
-            'workshop_type_id'=>request('workshop_type_id'),
-            'description'=>request('description'),
-            'address_id'=>DB::getPdo()->lastInsertedId()]);
+        $address = new Address;
+        $workshop = new Workshops;
+        $address->city_id=$request->city_id;
+        $address->street_name=$request->street_name;
+        $address->postal_code=$request->postal_code;
+        $address->building_number=$request->building_number;
+        $address->save();
+        $workshop->name=$request->name;
+        $workshop->workshop_type_id=$request->workshop_type_id;
+        $workshop->phone_number=$request->phone_number;
+        $workshop->email=$request->email;
+        $workshop->description=$request->description;
+        $workshop->address_id=$address->id;
+        $workshop->save();
+         /*Address::create([//$request->all()
+            'city_id'=>$request['city_id'],
+            'street_name'=>$request['street_name'],
+            'postal_code'=>$request['postal_code'],
+            'building_number'=>$request['building_number'],
+        ]);
+        Workshops::create([//$request->all()/*
+            'name'=>$request['name'],
+            'workshop_type_id'=>$request['workshop_type_id'],
+            'phone_number'=>$request['phone_number'],
+            'email'=>$request['email'],
+            'description'=>$request['description'],
+            'address_id'=>DB::getPdo()->lastInsertedId()]);*/
         return redirect('/account/workshops');
     }
 }
