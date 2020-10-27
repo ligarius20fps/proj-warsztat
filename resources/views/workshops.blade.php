03b8e9f412f0d67b4f7ac6ffa665ef1bb59c24fa
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-27 21:04:17 +0100

    Update workshops.blade.php

--- resources/views/workshops.blade.php
+++ resources/views/workshops.blade.php
@@ -3,5 +3,15 @@
 <div class="container">
      <h1>Moje warsztaty</h1>
      <a class="btn btn-primary" href="{{ '/account/workshops/new' }}">Dodaj warsztat</a>
+     <table class="table table-striped">
+     <thead></thead>
+     <tbody>
+ @foreach($workshops as $workshop)
+ <tr><td>
+    <a href="/workshop/{{ $workshop->id }}"> {{ $workshop->name }} </a>
+ </td></tr>
+ @endforeach
+ <tbody>
+ </table>
 </div>
 @endsection
