d879e3bc7d47b3e222b8676f89009be76efa632b
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-30 18:01:43 +0100

    Update workshop_page.blade.php

--- resources/views/workshop_page.blade.php
+++ resources/views/workshop_page.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
+    @foreach($workshops as $workshop) //to jest glupie
     <h1>{{ $workshop->name }}</h1>
     <p>{{ $workshop->description }}</p>
-    <div>
-        
-    </div>
+    @endforeach
+    <h3>Opinie</h3>
+    <h3>Umów się na wizytę</h3>
+    <button class="btn btn-primary">Umów się</button>
     </div>
 @endsection
