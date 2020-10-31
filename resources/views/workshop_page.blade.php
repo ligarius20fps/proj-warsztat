629c9ee4321a9e59b27595428cef0278d9534ca7
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-31 19:32:31 +0100

    Update workshop_page.blade.php

--- resources/views/workshop_page.blade.php
+++ resources/views/workshop_page.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-    @foreach($workshops as $workshop)
     <h1>{{ $workshop->name }}</h1>
+    <h5>{{ $workshop->workshop_type->name }}  |  <i class="fas fa-map-marker-alt"></i>{{ $workshop->address->city->name }}</h5>
     <p>{{ $workshop->description }}</p>
-    @endforeach
     <h3>Opinie</h3>
     <h3>Umów się na wizytę</h3>
     <button class="btn btn-primary">Umów się</button>
