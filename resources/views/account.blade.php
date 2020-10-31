59ea9c4eb529ee22f44b59d5f0f7509532eba77c
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-10-31 17:19:13 +0100

    Update account.blade.php

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -5,7 +5,8 @@
      <p>Użytkownik od {{ Auth::user()->created_at }}</p>
      <button class="btn btn-primary">Zmień hasło</button><br/><br/>
      @if(Auth::user()->user_type == 1)
-     <button class="btn btn-primary">Historia wizyt</button>
+     <a class="btn btn-primary" href="/account/customer">Edytuj dane kontaktowe</a>
+     <a class="btn btn-primary" href="/account/workshops/visits">Historia wizyt</a>
      @elseif(Auth::user()->user_type == 2)
      <a class="btn btn-primary" href="/account/workshops">Moje warsztaty</a>
      @endif
