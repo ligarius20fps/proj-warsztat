49bfbe0aad530b3a220858f9ee8e7b6ae12f68ff
WKogut98 <72746941+WKogut98@users.noreply.github.com>
2020-11-06 17:43:58 +0100

    Update visits.blade.php

--- resources/views/visits.blade.php
+++ resources/views/visits.blade.php
@@ -1,42 +1,67 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-     <h1>Historia wizyt</h1>
+     <h1>@if(Auth::user()->user_type==1)
+         Moje wizyty
+     @else
+        Wizyty warsztatu
+     @endif</h1>
+    <h4>Oczekiwane wizyty</h4>
      <table class="table table-striped">
      <thead></thead>
      <tbody>
  @foreach($visits as $visit)
- <tr><td>
-         @if(Auth::user()->user_type==1)
-    {{ $visit->workshop->name }}
-    @elseif(Auth::user()->user_type==2)
-    {{ $visit->customer->name }}
-    @endif
-     </td><td>{{ $visit->date }}</td>
-     <td>
-         @switch($visit->status)
-            @case(0)
-                Oczekiwanie na realizację
-                @break
-            @case(1)
-                W trakcie realizacji
-                @break
-            @case(2)
-                Zrealizowano
-                @break
-            @default
-                Nieznany status
-                @break
-        @endswitch
-     </td>
-     @if(Auth::user()->user_type==1)
-        <td><button class="btn btn-primary">Wystaw Ocenę</button></td></tr>
-     @elseif(Auth::user()->user_type==2)
-        <td><button class="btn btn-primary">Zmień Status</button></td></tr>
-     @endif
+         @if($visit->status==0)
+         <tr><td>
+            @if(Auth::user()->user_type==1)
+                {{ $visit->workshop->name }}
+            @elseif(Auth::user()->user_type==2)
+                {{ $visit->customer->name }}
+            @endif
+            </td><td>{{ $visit->date }}</td>
+            <td><a href="/visit/{{ $visit->id}}" class="btn btn-primary">Szczegóły</a></td></tr>
+         @endif
  @endforeach
- <tbody>
+    </tbody>
+    </table>
+    <h4>W trakcie realizacji</h4>
+     <table class="table table-striped">
+     <thead></thead>
+     <tbody>
+    @foreach($visits as $visit)
+        @if($visit->status==1)
+        <tr><td>
+            @if(Auth::user()->user_type==1)
+                {{ $visit->workshop->name }}
+            @elseif(Auth::user()->user_type==2)
+                {{ $visit->customer->name }}
+            @endif
+            </td><td>{{ $visit->date }}</td>
+            <td><a href="/visit/{{ $visit->id}}" class="btn btn-primary">Szczegóły</a></td></tr>
+        @endif
+ @endforeach
+    </tbody>
+    </table>
+    <h4>Zrealizowane</h4>
+    <table class="table table-striped">
+    <thead></thead>
+    <tbody>
+    @foreach($visits as $visit)
+        @if($visit->status==2)
+        <tr><td>
+            @if(Auth::user()->user_type==1)
+                {{ $visit->workshop->name }}
+            @elseif(Auth::user()->user_type==2)
+                {{ $visit->customer->name }}
+            @endif
+            </td><td>{{ $visit->date }}</td>
+            @if(Auth::user()->user_type==1)
+            <td><button class="btn btn-primary">Wystaw Ocenę</button></td>
+            @endif
+            <td><a href="/visit/{{ $visit->id}}" class="btn btn-primary">Szczegóły</a></td></tr>
+        @endif
+    @endforeach
+ </tbody>
  </table>
 </div>
 @endsection
-
